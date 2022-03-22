9520c9228d5b642aba78c562528adde31ad40968
Jonas Lalin <1ac6df24@gmail.com>
2022-03-22 21:32:28 +0100

    add test

--- tests/unet/test_model.py
+++ tests/unet/test_model.py
@@ -3,17 +3,29 @@ import tensorflow as tf
 from unet.model import unet
 
 
-def test_ronneberger_unet():
+def test_unet_with_valid_padding():
     model = unet(
         input_shape=(572, 572, 1),
         num_classes=2,
         padding="valid",
         filters=(64, 128, 256, 512, 1024),
     )
-    assert model.output_shape == (None, 388, 388, 2)
-
     with tf.device("/device:CPU:0"):  # tensorflow-metal fix
         g = tf.random.Generator.from_seed(1)
         inputs = g.uniform(shape=(2, 572, 572, 1))
     outputs = model(inputs)
     assert outputs.shape == (2, 388, 388, 2)
+
+
+def test_unet_with_same_padding():
+    model = unet(
+        input_shape=(512, 512, 3),
+        num_classes=3,
+        padding="same",
+        filters=(64, 128, 256, 512, 1024),
+    )
+    with tf.device("/device:CPU:0"):  # tensorflow-metal fix
+        g = tf.random.Generator.from_seed(1)
+        inputs = g.uniform(shape=(2, 512, 512, 3))
+    outputs = model(inputs)
+    assert outputs.shape == (2, 512, 512, 3)
